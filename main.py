8239f5137bbde8c539c1ffbd9e8125d0dc8356aa
nathanieldorn <nathaniel.svc@pm.me>
2025-04-26 14:27:19 -0400

    Add foward/back mvmt & update/draw groups

--- main.py
+++ main.py
@@ -1,34 +1,65 @@
 import pygame
 
-from pygame.time import Clock
 from constants import *
 from player import Player
 
-pygame.init()
+'''pygame.init()
 clock = pygame.time.Clock()
 dt = 0
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 starting_x = SCREEN_WIDTH / 2
 starting_y = SCREEN_HEIGHT / 2
-player1 = Player(starting_x, starting_y)
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = (updatable, drawable)
+player1 = Player(starting_x, starting_y)'''
 
-def game_loop():
+'''def game_loop():
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
-        player1.draw(screen)
+        for item in drawable:
+            drawable.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        player1.update(dt)
+        for item in updatable:
+            updatable.update(dt)'''
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    game_loop()
+    #game_loop()
+
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    starting_x = SCREEN_WIDTH / 2
+    starting_y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player1 = Player(starting_x, starting_y)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+        dt = clock.tick(60) / 1000
+        for upd in updatable:
+            upd.update(dt)
+        screen.fill("black")
+        for drw in drawable:
+            drw.draw(screen)
+        pygame.display.flip()
+
+
 
 if __name__ == "__main__":
     main()
